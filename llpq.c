9f758bc47120a1ad2c4ec2f8c45412b55884d358
abbenteprizer <abbe_g_93@hotmail.com>
2018-12-08 13:47:12 +0100

    added peek

--- llpq.c
+++ llpq.c
@@ -17,29 +17,29 @@ struct node *newQueue(int data, double priority) {
   return first;
 }
 
-void insert(struct node *head, int data, double priority) {
+void insert(struct node **head, int data, double priority) {
   struct node *new = newQueue(data, priority);
-  struct node *first = head;
+  struct node *first = *head;
 
   // Boundary condition where node is first in the list
-  if(priority <= (head->priority)) { // FIFO?
-    new->next = head;
-    head = new;
+  if(priority <= ((*head)->priority)) { // FIFO?
+    new->next = (*head);
+    (*head) = new;
     //    printf("we got here 1 and p = %f \n", priority);    
   } else {
-    while((head->next != NULL) && priority >((head->next)->priority)){
+    while(((*head)->next != NULL) && priority >(((*head)->next)->priority)){
       //      printf("we got here 2 and p = %f \n", priority);
-      head = head->next;
+      (*head) = (*head)->next;
     }
 
-    new->next = head->next;
-    head->next = new;
+    new->next = (*head)->next;
+    (*head)->next = new;
   }
   
 }
 
-void show(struct node *pq) {
-  struct node *it = pq;
+void show(struct node **pq) {
+  struct node *it = *pq;
   while(it != NULL) {
     printf("Node with data %d and priority %f\n", it->data, it->priority);
     it = it->next;
@@ -48,21 +48,27 @@ void show(struct node *pq) {
   
 }
 
-struct node* removePriorityMax(struct node *pq) {
-  struct node *ret = (struct node*)malloc(sizeof(ret));
-  ret->data = pq->data;
-  ret->priority = pq->priority;
-  ret->next = NULL;
-  printf(" did we get here? \n");
-
-  if(pq->next != NULL) {
-    printf("i stepped forward \n");
-    pq = pq->next; // Step forward
-    return ret;
+void removePriorityMax(struct node **pq) {
+  if(pq != NULL) {
+    struct node *tmp = *pq;
+    if((*pq)->next != NULL) {
+      *pq = (*pq)->next; // Step forward
+      free(tmp); // Remove the elem
+      return;
+    } else {
+      free(pq);
+      printf("list is now empty \n");
+    }
+    return;
+  } else {
+    printf("list is already empty\n");
   }
+}  
 
-  printf("list is now empty \n");
-  return ret;
+int peek(struct node **head) {
+  
+  return (*head)->data;
+  
 }
 
 
@@ -72,23 +78,30 @@ int main(int argc, char* argv[]) {
   struct node *pq = newQueue(5, 0.3);
 
   printf("now inserting in the queue\n");  
-  insert(pq, 3, 0.34);
-  insert(pq, 7, 2.38);
-  insert(pq, 7, 2.33);
-  insert(pq, 7, 1.38);    
+  insert(&pq, 3, 0.34);
+  insert(&pq, 7, 2.38);
+  insert(&pq, 7, 2.33);
+  insert(&pq, 7, 1.38);    
   
-  show(pq);
-
-  struct node *test;
-  printf("now for tested\n");  
-  test = removePriorityMax(pq);
-
-  printf("now for removed\n");
-  show(test);
+  show(&pq);
+  int test;
+  test = peek(&pq);
+  printf("now for how test looks\n");    
+  printf("test data is %d\n", test);
+  
+  printf("\n");    
+  printf("now for remove\n");  
+  removePriorityMax(&pq);
+  removePriorityMax(&pq);  
 
   printf("how does pq look?\n");
-  show(pq);
+  show(&pq);
 
+  test = peek(&pq);
+  printf("now for how test looks\n");    
+  printf("test data is %d\n", test);
+
+  
        
   return 0;
 }
